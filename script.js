9c2009ed06daf80a55344465244c83a29427af63
giginfee <olya.bernatski@gmail.com>
2023-10-24 21:32:34 +0300

    design

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let lenInput=document.getElementById("len_input")
 let generateButton=document.getElementById("generate_button")
+let resultDiv=document.getElementById("result")
 
-// let lenInput=form.len_input
+
+resultDiv.addEventListener("click",()=>{
+    if (resultDiv.innerText!=="")
+        navigator.clipboard.writeText(resultDiv.innerText);}
+)
 lenInput.addEventListener("input",()=>
     document.getElementById("len").innerText=lenInput.value)
 
 generateButton.addEventListener("click",showResult)
 
 function showResult(){
+    resultDiv.classList+=" pointer"
+
     let len= parseInt(document.getElementById("len").innerText)
     document.getElementById("result").innerText=generatePassword(len)
 
@@ -17,7 +24,7 @@ function generatePassword(len){
     let res=""
     let symbArr=getSymbolArr()
     for(let i=1;i<=len;i++){
-        res+=symbArr[getRandomInt(1,symbArr.length)]
+        res+=symbArr[getRandomInt(1,symbArr.length-1)]
     }
 return res
 }
@@ -25,9 +32,9 @@ return res
 function getSymbolArr(){
     let arr=[]
     for(let i=33;i<=125;i++){
-        if([34,39,44,46,58,59,96,124].indexOf(i)===-1)
+        if([34,39,44,46,58,59,96,124].indexOf(i)===-1) {
             arr.push(String.fromCharCode(i))
-    }
+        }    }
     return arr
 }
 function getRandomInt(min, max) {
@@ -35,3 +42,4 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
